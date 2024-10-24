067150811f1cf3eda9b5cc83f70684bd1b5093bd
sumyeom <wjstndus75@naver.com>
2024-10-24 10:57:46 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,37 +1,9 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        int receiveCola = 0;
-        int remainCola = 0;
-
-        int tempN = n;
-        
-        while(true){
-            if((tempN + remainCola >= a) && tempN < a){
-                receiveCola = ((tempN + remainCola) / a) * b;
-                int tempremainCola = remainCola;
-                remainCola = 0;
-                remainCola += (tempN + tempremainCola) % a;
-                System.out.println("receive2= "+receiveCola);
-                System.out.println("remain2 = "+remainCola);
-                tempN = receiveCola; 
-                answer += receiveCola;
-                System.out.println("tempN = "+tempN);
-            }else if(tempN + remainCola < a){
-                break;
-            }
-            receiveCola = (tempN/a) * b;
-            remainCola += tempN%a;
-            System.out.println("receive1 = "+receiveCola);
-            System.out.println("remain1 = "+remainCola);
-            if(receiveCola == 0 && remainCola <a){
-                break;
-            }
-            
-            answer += receiveCola;
-            tempN = receiveCola; 
-            System.out.println("tempN = "+tempN);
-            
+        while(n/a>0){
+            answer+= (n/a)*b;
+            n = ((n/a)*b) + (n%a);
         }
         
         return answer;
