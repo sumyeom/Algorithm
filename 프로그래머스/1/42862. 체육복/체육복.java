69d937eb3a26eb6ecc80c4de37c5205408f59848
sumyeom <wjstndus75@naver.com>
2024-10-28 16:31:28 +0900

    [level 1] Title: 체육복, Time: 0.06 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,37 +1,34 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] clothes = new int[n];
-        
-        for(int i=0;i<n;i++){
-            clothes[i] = 1;
+        int[] student = new int[n];
+        Arrays.fill(student, 1);
+        for(int i=0;i<lost.length;i++){
+            student[lost[i]-1]=0;
         }
         for(int i=0;i<reserve.length;i++){
-            clothes[reserve[i]-1]++;
+            student[reserve[i]-1]++;
         }
-        for(int i=0;i<lost.length;i++){
-            clothes[lost[i]-1]--;
-        }
-        
         for(int i=0;i<n;i++){
-            if(clothes[i]==0){
-                //앞에 볼라고했는데 첫번째인자이면?
-                if(i>0 && clothes[i-1] == 2){
-                    clothes[i]++;
-                    clothes[i-1]--;
-                }
-                else if(i < n - 1 && clothes[i+1] == 2){
-                    clothes[i]++;
-                    clothes[i+1]--;
+            // 도난당한 사람이면
+            if(student[i] == 0){
+                if(i>0 && student[i-1] == 2){
+                    student[i]++;
+                    student[i-1]--;
+                }else if(i<n-1 && student[i+1] == 2){
+                    student[i]++;
+                    student[i+1]--;
                 }
             }
         }
+        
         for(int i=0;i<n;i++){
-            if(clothes[i] == 1 || clothes[i]==2){
+            if(student[i] == 1 || student[i] == 2){
                 answer++;
             }
         }
-        
         return answer;
     }
 }
