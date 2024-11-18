b7c1c5fa1215ea642009091df136f6536dd36a83
sumyeom <wjstndus75@naver.com>
2024-11-18 11:20:59 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.34 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,22 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int n) {
-        long answer = 0;
-        long temp = (long)n;
-        ArrayList<Long> list = new ArrayList<Long>();
-        while(true){
-            if(temp == 0 ){
-                break;
-            }
-            list.add((long)temp%3);
-            temp = temp/3;
+        String answer = "";
+        while(n>0){
+            answer = answer + n%3;
+            n /=3;
+            System.out.println(answer);
         }
-        int mul = 1;
-        for(int i=list.size()-1;i>=0;i--){
-            answer += list.get(i)*mul;
-            mul *=3;
-        }
-        System.out.println(list.toString());
-        return (int)answer;
+        return Integer.parseInt(answer,3);
     }
 }
