af3025bf44acccf1f0a29f06263ef5cd245336f9
sumyeom <wjstndus75@naver.com>
2024-11-19 09:41:30 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,18 +1,16 @@
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        long[] dp = new long[n+1];
-        if( n <= 2){
-            return n;
-        }
+        int[] dp = new int[n+2];
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
-        
+        if(n<=2){
+            return dp[n]  % 1234567;
+        }
         for(int i=3;i<=n;i++){
             dp[i] = (dp[i-1]+dp[i-2])%1234567;
         }
-        
-        return dp[n];
+        return dp[n] % 1234567;
     }
 }
