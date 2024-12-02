c9ab4209a9fa16244376ca72a53bb99cd3aa2349
sumyeom <wjstndus75@naver.com>
2024-12-02 11:57:48 +0900

    [level 2] Title: H-Index, Time: 17.28 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        if(citations[0]>citations.length){
+            return citations.length;
+        }
+        int cnt = 0;
+        int h = 0;
+        for(int i=citations[citations.length-1];i>=0;i--){
+            cnt = 0;
+            for(int j=citations.length-1;j>=0;j--){
+                if(i<=citations[j]){
+                    cnt++;
+                }
+            }
+            if(cnt >= i){
+                h = Math.max(h,i);
+            }
+        }
+        return h;
+    }
+}
