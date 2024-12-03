dbd6477ad2f85e7714b9b0a99949df055e6ce889
sumyeom <wjstndus75@naver.com>
2024-12-03 09:59:46 +0900

    [level 2] Title: 할인 행사, Time: 44.27 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#qna) 
 
 ### 성능 요약
 
-메모리: 140 MB, 시간: 161.95 ms
+메모리: 115 MB, 시간: 44.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 03일 09:47:51
+2024년 12월 03일 09:59:44
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,27 +1,43 @@
 import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-int answer = 0;
-        int sum = 0;
-        for(int num : number){
-            sum += num;
+        int answer = 0;
+        int sum=0;
+        for(int num:number){
+            sum+= num;
         }
-        for(int i=0;i<discount.length-sum+1;i++){
-            String[] splitStr = Arrays.copyOfRange(discount,i,i+sum);
-            System.out.println();
-            List<String> list = Arrays.asList(splitStr);
-            boolean flag = true;
-            for(int j=0;j<want.length;j++){
-                int cnt = Collections.frequency(list,want[j]);
-                if(cnt<number[j]) {
-                    flag = false;
-                    break;
-                }
+        
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<sum;i++){
+            map.put(discount[i],map.getOrDefault(discount[i],0)+1);
+        }
+        
+        if(isValid(map,want,number)){
+            answer++;
+        }
+        
+        for(int i=sum;i<discount.length;i++){
+            String prev = discount[i-sum];
+            map.put(prev,map.get(prev)-1);
+            if(map.get(prev)==0){
+                map.remove(prev);
             }
-            if(flag){
+            
+            String curr = discount[i];
+            map.put(curr,map.getOrDefault(curr,0)+1);
+            
+            if(isValid(map,want,number)){
                 answer++;
             }
         }
         return answer;
+    }
+    public boolean isValid(Map<String,Integer> map, String[] want, int[] number){
+        for(int i=0;i<want.length;i++){
+            if(map.getOrDefault(want[i],0)<number[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
