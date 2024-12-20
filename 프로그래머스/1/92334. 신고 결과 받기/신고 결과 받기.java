019fc2b5afb7969b63ef1a7febe6a6b66c5c22ca
sumyeom <wjstndus75@naver.com>
2024-12-20 11:15:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 1043.43 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,37 +1,46 @@
+/*
+각 유저는 한번에 한명의 유저를 신고할 수 있음
+신고 횟수에 제한은 없어서 서로 다른 유저를 신고할 수 있다.
+한 유저를 여러번 신고는 할수있지만 동일한 유저에 대한 신고 횟수는 1회로 처리하게됨.
+k번 이상 신고된 유저는 정지 -> 해당 유저를 신고한 사람한테 알림이 간다.
+마지막에 날림
+*/
 import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-        LinkedHashMap<String,List<String>> map = new LinkedHashMap<>();
-        LinkedHashMap<String, Integer> idMap = new LinkedHashMap<>();
-        
-        for(int i=0;i<id_list.length;i++){
-            idMap.put(id_list[i],0);
-        }
+                int[] answer = new int[id_list.length];
+        // 신고당한 사람, 신고한 사람 리스트
+        Map<String, List<String>> idPeople = new HashMap<>();
+        // 신고 당한 사람, 신고 당한 횟수 map
+        Map<String, Integer> reportPeople = new HashMap<>();
+
+        // report 돌면서 저장
         for(int i=0;i<report.length;i++){
-            String[] strReport = report[i].split(" ");
-            map.computeIfAbsent(strReport[1], list -> new ArrayList<>());
-            if(!map.get(strReport[1]).contains(strReport[0])){
-                map.get(strReport[1]).add(strReport[0]);
+            String fromPeople = report[i].split(" ")[0];
+            String toPeople = report[i].split(" ")[1];
+            List<String> fromPeopleList = idPeople.get(toPeople) == null ? new ArrayList<>() : idPeople.get(toPeople);
+            //
+            if(!fromPeopleList.contains(fromPeople)) {
+                fromPeopleList.add(fromPeople);
+                reportPeople.put(toPeople, reportPeople.getOrDefault(toPeople, 0) + 1);
             }
+            idPeople.put(toPeople, fromPeopleList);
 
         }
-         // 신고 횟수가 k 이상인 유저를 찾아서 처리
-        for (int i = 0; i < id_list.length; i++) {
-            String user = id_list[i];
-            List<String> reporters = map.get(user);
 
-            // null이 아니고, 신고 횟수가 k 이상인 경우에만 처리
-            if (reporters != null && reporters.size() >= k) {
-                for (String reporter : reporters) {
-                    idMap.put(reporter, idMap.get(reporter) + 1);
+        //
+        for(String fromPeople : idPeople.keySet()){
+            int num = reportPeople.get(fromPeople);
+            if(num >= k) {// k번이상인지 확인
+                List<String> list = idPeople.get(fromPeople);
+                for(int i=0;i<id_list.length;i++){
+                    for(int j=0;j<list.size();j++){
+                        if(id_list[i].equals(list.get(j))){
+                            answer[i]++;
+                        }
+                    }
                 }
             }
-        }
-
-        // 결과를 answer 배열에 저장
-        for (int i = 0; i < id_list.length; i++) {
-            answer[i] = idMap.get(id_list[i]);
         }
         return answer;
     }
