6b4f3a0090502dbc35a03f15c4665da23c0759fe
sumyeom <wjstndus75@naver.com>
2025-04-01 20:00:51 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 6.39 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,6 +1,32 @@
+import java.util.*;
 class Solution {
-    public int solution(int[] mats, String[][] park) {
-        int answer = 0;
+        static int answer = 0;
+    public static int solution(int[] mats, String[][] park) {
+        Arrays.sort(mats);
+        for(int m = mats.length-1;m>=0;m--){
+            for(int x=0;x<=park.length-mats[m];x++){
+                for(int y=0;y<=park[0].length-mats[m];y++){
+                    if(find(x, y, mats[m], park)) answer = Math.max(answer, mats[m]);
+                }
+            }
+
+        }
+        
+        if(answer == 0) return -1;
         return answer;
+    }
+
+    public static boolean find(int x, int y, int mat, String[][] park){
+        for(int i = x;i<x+mat;i++){
+            for(int j=y;j<y+mat;j++){
+                if(i < 0 || i >=park.length || j <0 || j>=park[0].length){
+                    continue;
+                }
+                if(!park[i][j].equals("-1")){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
