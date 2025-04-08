e8df69a3055c1ce81961f4023228d8628bb536b5
sumyeom <wjstndus75@naver.com>
2025-04-08 14:38:05 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 2.10 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,57 +1,6 @@
-import java.util.*;
 class Solution {
-    //data -> code / date / maximum / remain
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        
-        List<int[]> list = new ArrayList<>();
-        
-        switch(ext){
-            case "code":
-                for(int i=0;i<data.length;i++){
-                    if(val_ext > data[i][0]){
-                        list.add(data[i]);
-                    }
-                }
-                break;
-            case "date":
-                for(int i=0;i<data.length;i++){
-                    if(val_ext > data[i][1]){
-                        list.add(data[i]);
-                    }
-                }
-                break;
-            case "maximum":
-                for(int i=0;i<data.length;i++){
-                    if(val_ext > data[i][2]){
-                        list.add(data[i]);
-                    }
-                }
-                break;
-            case "remain":
-                for(int i=0;i<data.length;i++){
-                    if(val_ext > data[i][3]){
-                        list.add(data[i]);
-                    }
-                }
-                break;
-        }     
-        
-        switch(sort_by){
-            case "code":
-                Collections.sort(list, (a, b) -> Integer.compare(a[0], b[0]));;
-                break;
-            case "date":
-                Collections.sort(list, (a, b) -> Integer.compare(a[1], b[1]));
-                break;
-            case "maximum":
-                Collections.sort(list, (a, b) -> Integer.compare(a[2], b[2]));
-                break;
-            case "remain":
-                Collections.sort(list, (a, b) -> Integer.compare(a[3], b[3]));
-                break;
-        }
-        
-        
-        return list.toArray(new int[list.size()][]);
+        int[][] answer = {};
+        return answer;
     }
 }
