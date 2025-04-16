28ba1070f70626ef4f1446b04a7e88df127e30c0
sumyeom <wjstndus75@naver.com>
2025-04-16 21:17:55 +0900

    Title: 문자 출력하기, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250416/문자 출력하기/print-text.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String c = sc.nextLine();
+
+        for(int i =0;i<8;i++){
+            System.out.print(c);
+        }
+    }
+}
