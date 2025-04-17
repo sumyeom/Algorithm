fddcd586fe1b4e5adfcc0d17a5b4b79630bd2ba5
sumyeom <wjstndus75@naver.com>
2025-04-17 17:51:29 +0900

    Title: 정상적인 구구단, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250417/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(j == N){
+                    System.out.print(i + " * " + j +" = "+(i*j));
+                }else{
+                    System.out.print(i + " * " + j +" = "+(i*j)+", ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
