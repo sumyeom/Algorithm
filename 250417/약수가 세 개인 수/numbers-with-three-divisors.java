c9a607c8b1cd7bdc6358e02f18ffb95de5fcf972
sumyeom <wjstndus75@naver.com>
2025-04-17 19:05:57 +0900

    Title: 약수가 세 개인 수, Time: 147ms, Memory: 10MB, Status: Passed - Codetree

--- 250417/약수가 세 개인 수/numbers-with-three-divisors.java
+++ 250417/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -8,7 +8,7 @@ public class Main {
         int cnt = 0;
         for(int i=start ;i<=end;i++){
             int num = 0;
-            for(int j=1;j<=i/2;j++){
+            for(int j=1;j<=i;j++){
                 if(i%j==0){
                     num++;
                 }
