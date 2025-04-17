70d5c91388c787fb0da9d5dc534b17f8c32ce591
sumyeom <wjstndus75@naver.com>
2025-04-18 02:43:20 +0900

    Title: 특정 원소들의 합, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250418/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[4][4];
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int cnt = 0;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                cnt+=arr[i][j];
+            }
+        }
+        System.out.println(cnt);
+    }
+}
