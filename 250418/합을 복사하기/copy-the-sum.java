8ce05134f6d17ecfb85de79624b6933383fa7368
sumyeom <wjstndus75@naver.com>
2025-04-18 22:53:15 +0900

    Title: 합을 복사하기, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250418/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        a = a+b+c;
+        b = a;
+        c = a;
+        System.out.println(a+ " " + b+ " "+c);
+    }
+}
