19d24f883e6ef368f124bc76ad5b619cf2d002a8
sumyeom <wjstndus75@naver.com>
2025-04-18 22:48:52 +0900

    Title: 데이터 교환, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250418/데이터 교환/exchange-data.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int tempA = 5;
+        int tempB = 6;
+        int tempC = 7;
+
+        b = tempA;
+        c = tempB;
+        a = tempC;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
