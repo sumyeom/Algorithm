90af57b57904bfd0550fddcb6067869c1ea35714
sumyeom <wjstndus75@naver.com>
2025-04-18 23:19:50 +0900

    Title: 문자열 받아 출력, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250418/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+
+        System.out.println(S);
+    }
+}
