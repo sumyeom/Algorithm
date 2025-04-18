ffa3f0fbf84e536eeef70b8dc928e3f67b68dcdb
sumyeom <wjstndus75@naver.com>
2025-04-18 23:01:55 +0900

    Title: 실수 받아 그대로 출력, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250418/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+
+        System.out.printf("%.2f",a);
+    }
+}
