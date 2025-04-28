8c538eea776bc9928c7375858899f4d9c3ba0218
sumyeom <wjstndus75@naver.com>
2025-04-28 11:46:13 +0900

    Title: 삼항연산자, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250428/삼항연산자/ternary-operator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a == 100){
+            System.out.println("pass");
+        }else{
+            System.out.println("failure");
+        }
+    }
+}
