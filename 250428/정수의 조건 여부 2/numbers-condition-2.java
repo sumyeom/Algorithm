2800aaa23a1bbfe127417e4b9efa28cb7a8e275e
sumyeom <wjstndus75@naver.com>
2025-04-28 12:04:23 +0900

    Title: 정수의 조건 여부 2, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250428/정수의 조건 여부 2/numbers-condition-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a==5){
+            System.out.println("A");
+        }else if(a%2==0){
+            System.out.println("B");
+        }
+    }
+}
