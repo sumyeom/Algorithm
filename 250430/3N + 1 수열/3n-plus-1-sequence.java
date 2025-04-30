d7fa0db74070c62942a8736ddb35545064979ed4
sumyeom <wjstndus75@naver.com>
2025-04-30 20:18:56 +0900

    Title: 3N + 1 수열, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250430/3N + 1 수열/3n-plus-1-sequence.java
+++ 250430/3N + 1 수열/3n-plus-1-sequence.java
@@ -5,13 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int cnt = 0;
-        while(n>1){
+        while(true){
+            if(n==1){
+                break;
+            }
             if(n%2==0){
-                n%=2;
+                n/=2;
             }else{
                 n = n*3+1;
             }
             cnt++;
+            
         }
         System.out.println(cnt);
     }
