ccff256414c37cca4b946ce9ac4a0e4f3fb9ff53
sumyeom <wjstndus75@naver.com>
2025-04-30 17:04:58 +0900

    Title: 369 게임, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250430/369 게임/369-game.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            if(i%3==0 || (i/10 == 3||i/10==6||i/10==9) || (i%10==3||i%10==6||i%10==9)){
+                System.out.print(0+" ");
+            }else{
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
