51446f6a8672052ce67261d02bba1f354eebb56f
sumyeom <wjstndus75@naver.com>
2025-04-30 18:52:43 +0900

    Title: 친근하지 않은 수, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250430/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        for(int i=1;i<=n;i++){
+            if(i%2==0||i%3==0||i%5==0){
+                cnt++;
+            }
+        }
+        System.out.println(n-cnt);
+    }
+}
