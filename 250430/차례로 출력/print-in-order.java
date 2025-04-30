f1b2d0824625e2789dc5249717221ee37a4b79e9
sumyeom <wjstndus75@naver.com>
2025-04-30 14:41:34 +0900

    Title: 차례로 출력, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250430/차례로 출력/print-in-order.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        for(int i = 5 ;i<=17;i++){
+            System.out.print(i + " ");
+        }
+    }
+}
