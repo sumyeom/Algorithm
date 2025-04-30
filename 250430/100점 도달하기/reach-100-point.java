05a98713632b5ae1061af11573c8715810dd5a7b
sumyeom <wjstndus75@naver.com>
2025-04-30 16:55:57 +0900

    Title: 100점 도달하기, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 250430/100점 도달하기/reach-100-point.java
+++ 250430/100점 도달하기/reach-100-point.java
@@ -12,8 +12,10 @@ public class Main {
                 System.out.print("B ");
             }else if(i>=70){
                 System.out.print("C ");
-            }else if(i>-60){
+            }else if(i>=60){
                 System.out.print("D ");
+            }else{
+                System.out.print("F ");
             }
         }
     }
