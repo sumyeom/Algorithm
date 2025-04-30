98deb5772e55558918aacf35a2f78dc1cbfcb2d1
sumyeom <wjstndus75@naver.com>
2025-04-30 13:46:42 +0900

    Title: 굉장한 수, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250430/굉장한 수/amazing-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if(N%2==1 && N%3==0){
+            System.out.println("true");
+        }else if(N%2==0&&N%5==0){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
