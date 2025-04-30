ee31325a293e68700de3497e22201be4d9815481
sumyeom <wjstndus75@naver.com>
2025-04-30 17:22:00 +0900

    Title: A부터 B까지의 합, Time: 108ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250430/A부터 B까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+        for(int i=a;i<=b;i++){
+            sum+=i;
+        }
+
+        System.out.println(sum);
+    }
+}
