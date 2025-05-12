9b2701d61fd6daefce40071bb28f98314af3c397
sumyeom <wjstndus75@naver.com>
2025-05-12 16:06:41 +0900

    Title: 별표 출력하기 8, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250512/별표 출력하기 8/print-star-8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1;i<=N;i++){
+            if(i%2==1){
+                System.out.println("*");
+                continue;
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
