70978a63127764c31f4a7db3d1129a405f21bf2d
sumyeom <wjstndus75@naver.com>
2025-07-28 11:39:24 +0900

    [Silver II] Title: 연속합, Time: 656 ms, Memory: 108640 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,27 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
-        int[] arr = new int[N+1];
-        int[] dp = new int[N+1];
-
-        for(int i=1;i<=N;i++){
+        int[] arr = new int[N];
+        for(int i=0;i<N;i++){
             arr[i] = sc.nextInt();
         }
-
-        dp[0] = Integer.MIN_VALUE;
-        dp[1] = arr[1];
-        if(N <= 1){
-            System.out.println(dp[N]);
-            return;
+        
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+        
+        for(int i=1;i<N;i++){
+            currentSum = Math.max(currentSum + arr[i], arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
         }
-
-        for(int i=2;i<=N;i++){
-            dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
-        }
-
-        Arrays.sort(dp);
-        System.out.println(dp[N]);
+        
+        System.out.println(maxSum);
     }
 
 }
