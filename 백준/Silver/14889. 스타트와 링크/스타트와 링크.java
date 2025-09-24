63f4febfbf55061c9f2d91e71f7ce9e839eee6df
sumyeom <wjstndus75@naver.com>
2025-09-24 18:00:18 +0900

    [Silver I] Title: 스타트와 링크, Time: 508 ms, Memory: 43236 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,80 +1,57 @@
+
+import java.io.IOException;
 import java.util.*;
 
-/**
- * 백준 1759
- * 브루트포스 알고리즘
- * 백트래킹
- */
 public class Main {
     static int N;
-    static boolean[] visited;
-    static int[][] force;
-    static List<Integer> list;
-    static int min = Integer.MAX_VALUE;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        visited = new boolean[N];
-        force = new int[N][N];
-        list = new ArrayList<>();
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                force[i][j] = sc.nextInt();
-            }
-        }
+    static boolean[] selected;
+    static int[][] map;
+    static int answer = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+       Scanner sc = new Scanner(System.in);
 
-        dfs(0);
-        System.out.println(min);
+       N = sc.nextInt();
+       selected = new boolean[N];
+
+       // 능력 입력
+       map = new int[N][N];
+       for(int i=0;i<N;i++){
+           for(int j=0;j<N;j++){
+               map[i][j] = sc.nextInt();
+           }
+       }
+
+       dfs(0, 0);
+       System.out.println(answer);
     }
 
-    public static void dfs(int index){
-        int start=0;
-        int link = 0;
-        boolean[] check = new boolean[N];
-        List<Integer> another = new ArrayList<>();
-        if(list.size() == (N/2)){
-            for(int i=0;i<list.size();i++){
-                for(int j=i+1;j<list.size();j++){
-                    int a = list.get(i);
-                    int b = list.get(j);
-                    start += force[a][b];
-                    start += force[b][a];
-                    check[a] = true;
-                    check[b] = true;
-                }
-            }
-
+    public static void dfs(int start, int cnt){
+        List<Integer> teamStart = new ArrayList<>();
+        List<Integer> teamLink = new ArrayList<>();
+        int sumStart = 0, sumLink = 0;
+        if(cnt == N/2){
             for(int i=0;i<N;i++){
-                if(!check[i]){
-                    another.add(i);
+                if(selected[i]){
+                    teamStart.add(i);
+                }else{
+                    teamLink.add(i);
                 }
             }
 
-            for(int i=0;i<another.size();i++){
-                for(int j=i+1;j<another.size();j++){
-                    int a = another.get(i);
-                    int b = another.get(j);
-                    link += force[a][b];
-                    link += force[b][a];
+            for(int i=0;i<teamStart.size();i++){
+                for(int j=i+1;j<teamStart.size();j++){
+                    sumStart += map[teamStart.get(i)][teamStart.get(j)] + map[teamStart.get(j)][teamStart.get(i)];
+                    sumLink += map[teamLink.get(i)][teamLink.get(j)] +  map[teamLink.get(j)][teamLink.get(i)];
                 }
             }
-
-
-            min = Math.min(Math.abs(start-link), min);
+            answer = Math.min(answer, Math.abs(sumStart - sumLink));
             return;
         }
-        for(int i=index; i<N;i++){
-            if(!visited[i]){
-                visited[i] = true;
-                list.add(index);
-                dfs(i+1);
-                visited[i] = false;
-                list.remove(list.size()-1);
-            }
-        }
 
+        for(int i=start;i<N;i++){
+            selected[i] = true;
+            dfs(i+1, cnt+1);
+            selected[i] = false;
+        }
     }
 }
-
-
-
