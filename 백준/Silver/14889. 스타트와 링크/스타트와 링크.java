402cfcb327a824d8cca6efa461c177b407b2ab26
sumyeom <wjstndus75@naver.com>
2025-09-24 18:07:46 +0900

    [Silver I] Title: 스타트와 링크, Time: 436 ms, Memory: 21416 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -26,32 +26,33 @@ public class Main {
     }
 
     public static void dfs(int start, int cnt){
-        List<Integer> teamStart = new ArrayList<>();
-        List<Integer> teamLink = new ArrayList<>();
         int sumStart = 0, sumLink = 0;
         if(cnt == N/2){
-            for(int i=0;i<N;i++){
-                if(selected[i]){
-                    teamStart.add(i);
-                }else{
-                    teamLink.add(i);
+            for(int i=0;i<N-1;i++) {
+                for(int j=i+1;j<N;j++){
+                    if(selected[i] && selected[j]){
+                        sumStart += map[i][j] + map[j][i];
+                    }else if(!selected[i] && !selected[j]){
+                        sumLink += map[i][j] + map[j][i];
+                    }
                 }
             }
-
-            for(int i=0;i<teamStart.size();i++){
-                for(int j=i+1;j<teamStart.size();j++){
-                    sumStart += map[teamStart.get(i)][teamStart.get(j)] + map[teamStart.get(j)][teamStart.get(i)];
-                    sumLink += map[teamLink.get(i)][teamLink.get(j)] +  map[teamLink.get(j)][teamLink.get(i)];
-                }
+            int val = Math.abs(sumStart - sumLink);
+            if(val == 0){
+                System.out.println(0);
+                System.exit(0);
             }
             answer = Math.min(answer, Math.abs(sumStart - sumLink));
             return;
         }
 
         for(int i=start;i<N;i++){
-            selected[i] = true;
-            dfs(i+1, cnt+1);
-            selected[i] = false;
+            if(!selected[i]){
+                selected[i] = true;
+                dfs(i+1, cnt+1);
+                selected[i] = false;
+            }
+
         }
     }
 }
