b78a577f375444c3b8b6bebfa923baa685192fa1
sumyeom <wjstndus75@naver.com>
2026-04-06 09:47:59 +0900

    [level 1] Title: 카드 뭉치, Time: 0.68 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,22 +1,28 @@
+import java.util.*;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "Yes";
-        int[] int1 = new int[cards1.length];
-        int[] int2 = new int[cards2.length];
-        int index1=0;
-        int index2=0;
-        // goal 배열을 순회하며 cards1, cards2에서 일치하는지 확인
-        for (int i = 0; i < goal.length; i++) {
-            if (index1 < cards1.length && goal[i].equals(cards1[index1])) {
-                index1++;
-            } else if (index2 < cards2.length && goal[i].equals(cards2[index2]))            {
-                index2++;
-            } else {
-                answer = "No";  // cards1이나 cards2에 없는 순서라면 "No"
+        String answer = "No";
+        // ArrayDeque로 goal 큐가 다 빠질때까지확인
+        ArrayDeque<String> queue1 = new ArrayDeque<>(Arrays.asList(cards1));
+        ArrayDeque<String> queue2 = new ArrayDeque<>(Arrays.asList(cards2));
+        ArrayDeque<String> goalQueue = new ArrayDeque<>(Arrays.asList(goal));
+        
+        while(!goalQueue.isEmpty()){
+            if(!queue1.isEmpty() && queue1.peekFirst().equals(goalQueue.peekFirst())){
+                queue1.pollFirst();
+                goalQueue.pollFirst();
+            }else if(!queue2.isEmpty() && queue2.peekFirst().equals(goalQueue.peekFirst())){
+                queue2.pollFirst();
+                goalQueue.pollFirst();
+            }else{
                 break;
             }
         }
-
+        if(goalQueue.isEmpty()){
+            
+            answer = "Yes";
+        }
+        
         return answer;
     }
 }
